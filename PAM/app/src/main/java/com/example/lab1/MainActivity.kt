ffd598e7ee10db69753e72bfd0632d4ed515a5ae
Sandu Vlad <vladsand2@gmail.com>
2022-09-11 16:42:15 +0300

    Final (sper)

--- PAM/app/src/main/java/com/example/lab1/MainActivity.kt
+++ PAM/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         height = findViewById(R.id.height_id)
         weight = findViewById(R.id.weight_id)
         result_field = findViewById(R.id.result_text_id)
